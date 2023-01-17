6d2f4253933380c6af010eb2c6bebbf311407bff
Alex Tsafos <atsaf98@gmail.com>
2023-01-17 19:05:14 +0200

    Script to run all

--- src/main/scala/SparkSession.scala
+++ src/main/scala/SparkSession.scala
@@ -1,14 +1,13 @@
-/* SparkSessionTest.scala */
+/* AdvancedDBBenchmarker.scala */
 import org.apache.spark.sql.SparkSession
 
-object SparkSessionTest {
+object AdvancedDBBenchmarker {
   def main(args: Array[String]) {
-    val logFile = "/home/user/spark-3.1.3-bin-hadoop2.7/README.md" // Should be some file on your system
+    val logFile = "hdfs://192.168.0.1:9000/oute_enas.txt" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val logData = spark.read.textFile(logFile)
+    val context = logData.first()
+    println(context)
     spark.stop()
   }
 }
