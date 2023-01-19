e6ec03d02198139bd4f62947fb322c9e7a988e13
Alex Tsafos <atsaf98@gmail.com>
2023-01-19 23:09:54 +0200

    Queries 1 through 4 and file output

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,8 +14,8 @@ jar="advanced-db-taxi-routes_2.12-1.0.jar"
 class="AdvancedDBBenchmarker"
 deployMode="client"
 
-firstResult="/home/user/ADB-code/results/first_result"
-secondResult="/home/user/ADB-code/results/second_result"
+firstResult="/home/user/ADB-code/results/one-worker"
+secondResult="/home/user/ADB-code/results/two-workers"
 
 # Build Scala, put jar to hdfs
 cd /home/user/ADB-code
@@ -30,6 +30,11 @@ $SPARK_HOME/bin/spark-submit \
 --class $class --master spark://$masterIP:$masterSparkPort --deploy-mode $deployMode \
 hdfs://$masterIP:$masterHDFSPort//jars/$jar $firstResult
 
+for i in 1 2 3 3RDD 4 5
+do
+    mv -f  $firstResult/Query${i}/part* $firstResult/Query${i}.csv
+    rm -rf $firstResult/Query${i}
+done
 # Latex table
 
 # Two workers
@@ -42,5 +47,11 @@ ssh user@snf-34140 $secondWorkerCommand
 $SPARK_HOME/bin/spark-submit \
 --class $class --master spark://$masterIP:$masterSparkPort --deploy-mode $deployMode \
 hdfs://$masterIP:$masterHDFSPort//jars/$jar $secondResult
+
+for i in 1 2 3 3RDD 4 5
+do
+    mv -f  $secondResult/Query${i}/part* $secondResult/Query${i}.csv
+    rm -rf $secondResult/Query${i}
+done
 
 # Latex table
--- src/main/scala/AdvancedDBBenchmarker.scala
+++ src/main/scala/AdvancedDBBenchmarker.scala
@@ -11,35 +11,99 @@ object AdvancedDBBenchmarker {
 
     val sparkMasterIpPort = "192.168.0.1:9000"
 
-    val taxiTripsDf = spark.read.parquet(s"hdfs://${sparkMasterIpPort}//data/tripdata/yellow_tripdata_2022-*.parquet")
+    val yellowDF = spark.read.parquet(s"hdfs://${sparkMasterIpPort}//data/tripdata/yellow_tripdata_2022-*.parquet")
     val zoneLookupsDf = spark.read.option("header","true").csv(s"hdfs://${sparkMasterIpPort}//data/taxi+_zone_lookup.csv")
-    
-    val taxiTripsRdd = taxiTripsDf.rdd
-    val zoneLookupsRdd = zoneLookupsDf.rdd
+
 
     zoneLookupsDf.createOrReplaceTempView("Zone_Lookups")
-    taxiTripsDf.createOrReplaceTempView("Taxi_Trips")
+    yellowDF.na.drop().createOrReplaceTempView("Taxi_Trips_Temp")
+    
+    // Add the zone names as columns and drop data not in the first six months of 2022
+    val taxiTripsDf = spark.sql("""SELECT PU.Zone as PUZone, Drop.Zone as DOZone, Taxi_Trips_Temp.* 
+                            FROM Taxi_Trips_Temp INNER JOIN Zone_Lookups as Drop ON Drop.LocationID == DOLocationID
+                            INNER JOIN Zone_Lookups PU ON PU.LocationID == PULocationID
+                            WHERE MONTH(tpep_pickup_datetime) between 1 and 6
+                            AND MONTH(tpep_dropoff_datetime) between 1 and 6
+                            AND PULocationID != 265
+                            AND PULocationID != 264
+                            AND DOLocationID != 265
+                            AND DOLocationID != 264""")
 
-    query1()
+    val zoneLookupsRdd = zoneLookupsDf.rdd
+    val taxiTripsRdd = taxiTripsDf.rdd
+
+    taxiTripsDf.createOrReplaceTempView("Taxi_Trips")
+    
+    // query1()
+    // query2()
+    query3SQL()
+    query3RDD()
 
     spark.stop()
 
       def query1() : Unit = {
-        // val pw = new PrintWriter(new File(s"${args(0)}"))
-        val batteryParkDF = spark.sql("SELECT LocationID FROM Zone_Lookups WHERE Zone  == 'Battery Park' ")
-        val batteryParkID = batteryParkDF.first().getString(0)
-
         val maxTipAmountDF = spark.sql(s"""SELECT MAX(Tip_amount) as max_tip_amount 
                                         FROM Taxi_Trips 
-                                        WHERE DOLocationID  == $batteryParkID AND MONTH(tpep_pickup_datetime) == 3""")
+                                        WHERE DOZone  == 'Battery Park' AND MONTH(tpep_pickup_datetime) == 3""")
         maxTipAmountDF.createOrReplaceTempView("Max_Tip")
 
-        val query1DF = spark.sql(s"""SELECT VendorID, PULocationID, tpep_pickup_datetime, Trip_distance, Total_amount, max_tip_amount 
+        val query1DF = spark.sql(s"""SELECT VendorID, PUZone, DOZone, tpep_pickup_datetime, Trip_distance, Total_amount, max_tip_amount 
                                   FROM Taxi_Trips INNER JOIN Max_Tip ON Tip_amount == max_tip_amount
-                                  WHERE DOLocationID  == $batteryParkID AND MONTH(tpep_pickup_datetime) == 3""")
-        // pw.close()
-        query1DF.write.mode(SaveMode.Overwrite).csv(s"${args(0)}")
+                                  WHERE DOZone  == 'Battery Park' AND MONTH(tpep_pickup_datetime) == 3""")
+        query1DF.show()
+        query1DF.repartition(1).write.option("header","true").mode("overwrite").csv(s"${args(0)}/Query1")
+      }
 
+      def query2() : Unit = {
+        val maxTollAmountperMonth = spark.sql(s"""SELECT MONTH(tpep_pickup_datetime) as month, MAX(Tolls_amount) as max_tolls_amount 
+                                        FROM Taxi_Trips 
+                                        GROUP BY MONTH(tpep_pickup_datetime)
+                                        HAVING max_tolls_amount > 0""")
+        maxTollAmountperMonth.createOrReplaceTempView("Max_Tolls")
+
+        val query2DF = spark.sql(s"""SELECT month, VendorID, PUZone, DOZone, tpep_pickup_datetime, Trip_distance, Total_amount, max_tolls_amount 
+                                  FROM Taxi_Trips INNER JOIN Max_Tolls ON Tolls_amount == max_tolls_amount
+                                  ORDER BY month""")
+        query2DF.show()
+        query2DF.repartition(1).write.option("header","true").mode("overwrite").csv(s"${args(0)}/Query2")
+      }
+
+      def query3SQL() : Unit = {
+        val query3DF = spark.sql(s"""SELECT MONTH(tpep_pickup_datetime) as trip_month, (CASE WHEN DAY(tpep_pickup_datetime) > 15 THEN 2 ELSE 1 END) as half_of_the_month, AVG(Trip_distance) as average_trip_distance
+                                  FROM Taxi_Trips
+                                  WHERE PULocationID != DOLocationID
+                                  GROUP BY trip_month, half_of_the_month
+                                  ORDER BY trip_month, half_of_the_month""")
+        query3DF.show()
+        query3DF.repartition(1).write.option("header","true").mode("overwrite").csv(s"${args(0)}/Query3")
+      }
+
+      def query3RDD() : Unit = {
+        val rddColumns = taxiTripsDf.columns
+        val puDatetimeIndex = rddColumns.indexOf("tpep_pickup_datetime")
+        val puLocationIDIndex = rddColumns.indexOf("PULocationID")
+        val doLocationIDIndex = rddColumns.indexOf("DOLocationID")
+        val tripDistanceIDIndex = rddColumns.indexOf("trip_distance")
+
+
+        def rowParser(row : org.apache.spark.sql.Row) = {
+          val rowDate = row(puDatetimeIndex).toString.split(' ')(0)
+          val rowDateDay = rowDate.split('-')
+          val month = rowDateDay(1).toInt
+          val day = rowDateDay(2).toInt
+          val tripDistance = row(tripDistanceIDIndex).toString.toFloat
+          val halfOfTheMonth = if (day > 15) { 2 } else { 1 }
+          
+          ((month, halfOfTheMonth), (tripDistance, 1))
+        }
+        val query3RDD = taxiTripsRdd.filter(row => row(puLocationIDIndex) != row(doLocationIDIndex))
+                                          .map(row => rowParser(row))
+                                          .reduceByKey((key1 , key2) => 
+                                          ((key1._1*key1._2 + key2._1*key2._2)/(key1._2+key2._2), key1._2+key2._2))
+
+        query3RDD.foreach(row => print(s"${row._1._1}, ${row._1._2}, ${row._2._1}\n"))
+
+        query3RDD.map(row => (row._1._1, row._1._2, row._2._1)).repartition(1).saveAsTextFile(s"${args(0)}/Query3RDD")
       }
   }
 
